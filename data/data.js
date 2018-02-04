1bf2f8f2818843c24b2a626e5afc3c9c28cf1bd9
Adam Tombleson <rekarnar@gmail.com>
2018-02-04 16:19:17 +0545

    setup

---
+++ data/data.js
@@ -0,0 +1,59 @@
+export default [{
+	name: 'Baked n fresh',
+	type: '',
+	location: '27.68354/85.31197',
+	date: '04-02-2018',
+	price: 737,
+	items: [{
+		type: 'coffee',
+		quality: 0.5,
+	}, {
+		type: 'breakfast',
+		quality: 0.7,
+		quantity: 0.7,
+	}],
+}, {
+	name: 'Himalayian beanz',
+	type: '',
+	location: '27.67916/85.30783',
+	date: '04-01-2018',
+	price: 450,
+	items: [{
+		type: 'coffee',
+		quality: 0.8,
+	}, {
+		type: 'breakfast',
+		quality: 0.8,
+		quantity: 0.9,
+	}],
+}, {
+	name: 'Cafe Soma',
+	type: '',
+	location: '27.6786056,85.3099393',
+	date: '20-01-2018',
+	price: 550,
+	items: [{
+		type: 'coffee',
+		quality: 0.5,
+	}, {
+		type: 'breakfast',
+		quality: 0.7,
+		quantity: 0.8,
+	}],
+}, {
+	name: 'Busy Bean',
+	type: '',
+	location: '27.6772368,85.3091066',
+	date: '25-01-2018',
+	price: 0,
+	items: [{
+		type: 'coffee',
+		quality: 0.6,
+	}, {
+		type: 'breakfast',
+		quality: 0.5,
+		quantity: 0.5,
+	}],
+}]
+
+
