6cd2f07d68c2d0e83ca8a3cd5154b8c9ab0f57cf
pombam <PombaM@users.noreply.github.com>
2018-02-24 15:58:07 +0545

    Data intial (#1)

--- src/data/dataManager.js
+++ src/data/dataManager.js
@@ -1,10 +1,27 @@
 import staticData from './static'
+import placesData from './placesData'
+
+// get visited data from somewhere????
+const visited = []
 
 const fetchData = {
 	fetch() {
 		// console.log('this should reutrn a probmise that gets our data', staticData);
 
-		return Promise.resolve(staticData)
+		const updatedData = placesData.results.map((place) => {
+			const {lat, lng} = place.geometry.location
+			const beenHere = visited.find(i => i.location === `${lat},${lng}`)
+
+			if (beenHere) {
+				place.visited = beenHere
+			} else {
+				place.visited = false
+			}
+
+			return place
+		})
+
+		return Promise.resolve(updatedData)
 	},
 
 }
